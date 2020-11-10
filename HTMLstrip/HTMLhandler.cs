9be1d4f9625ec1c5165eb9439986c2f014034522
Fábio Pacheco <38566316+Fasp96@users.noreply.github.com>
2020-11-10 16:59:08 +0000

    HTMLstrip working in striping and saving

---
+++ HTMLstrip/HTMLhandler.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace HTMLstrip
+{
+    class HTMLhandler
+    {
+
+        public string StripHTML(string filename)
+        {
+            if(!ValidateFilename(filename)) return "Not valid filename";
+
+            else
+            {
+                string fileContent = ReadHTML(filename);
+                return RemoveHTML(fileContent);
+            }
+            
+        }
+
+        //Method to verify if is a .html file and if it it exists and returns true or false
+        private bool ValidateFilename(string filename)
+        {
+            return (File.Exists(filename) && filename.Split('.').Last().Equals("html"));
+        }
+
+        //Method to read the .html file
+        private string ReadHTML(string filename)
+        {
+            return File.ReadAllText(filename);
+        }
+
+        //Method to remove HTML tags and multiple blank spaces
+        private string RemoveHTML(string content)
+        {
+            string output;
+
+            //get rid of HTML tags
+            output = Regex.Replace(content, "<[^>]*>", string.Empty);
+
+            //get rid of multiple blank lines
+            output = Regex.Replace(output, @"^\s*$\n", string.Empty, RegexOptions.Multiline);
+
+            return output;
+        }
+
+        //Method to save result in a .txt file
+        public bool SaveResult (string directory, string result)
+        {
+            System.IO.File.WriteAllText(directory.Substring(0, directory.LastIndexOf(".")+1)+"txt", result);
+
+            return true;
+        }
+    }
+}
---
+++ HTMLstrip/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+//D:\\Users\\fasp\\OneDrive - Universidade da Madeira\\UMa\\Mestrado\\Engenharia de Software\\4. Projeto\\HTMLstrip\\HTML-exercise.html
+namespace HTMLstrip
+{
+    class Program
+    {
+        private const string instructionsMessage = 
+            "** Alows users to extract blocks of text from an HTML file or string. **\n\n" +
+            "For example:\n\n" +
+            "\t? D:\\Users\\fasp\\Documents\\HTML-exercise.html (file directory)\n\n" +
+            "\t? \"<h1>Hello World!!</h1>\" (simple string)\n\n";
+
+        private const string savingMessage = 
+            "\n\n ***Save the text extracted in a .txt file? (Press Y to save or any key to continue)***";
+
+        static void Main(string[] args)
+        {
+            HTMLhandler Htmlhandler = new HTMLhandler();
+
+            Console.WriteLine("HTMLstrip - version 0.1");
+            Console.WriteLine(instructionsMessage);
+
+            for (; ; )
+            {
+                //Input
+                Console.Write("? ");
+                string input = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(input)) break;
+
+                //Shows the result
+                Console.WriteLine("\n\nText extracted from {0}:\n", input);
+                string result = Htmlhandler.StripHTML(input);
+                Console.WriteLine(result);
+
+                //Saving Section
+                Console.WriteLine(savingMessage);
+                if (Console.ReadKey().Key == ConsoleKey.Y)
+                    Console.Clear();
+
+                    if (Htmlhandler.SaveResult(input, result))
+                        Console.WriteLine("File sucessfully saved in the same directory as the submitted file\n\n");
+                    else
+                        Console.WriteLine("Something went wrong while saving file\n\n");
+
+            }
+        }
+    }
+}
